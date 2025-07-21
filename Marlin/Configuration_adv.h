26ff73fffa83a00da15839b1734d2754c3c35b71
galmiklos <miklos.gal@gmail.com>
2025-07-21 10:31:50 -0500

    Configured Z Steppers Auto-Alignment

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1036,7 +1036,7 @@
    * If not defined, probe limits will be used.
    * Override with 'M422 S<index> X<pos> Y<pos>'.
    */
-  #define Z_STEPPER_ALIGN_XY { {  10, 190 }, { 100,  10 }, { 190, 190 } }
+  #define Z_STEPPER_ALIGN_XY { {  70, 70 }, { 180,  70 }, { 180, 180 } , { 70, 180 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
@@ -1058,7 +1058,7 @@
    *               | 1   2 | 2   3 | 3   4 | 4   1 |
    */
   #ifndef Z_STEPPER_ALIGN_XY
-    //#define Z_STEPPERS_ORIENTATION 0
+    #define Z_STEPPERS_ORIENTATION 0
   #endif
 
   /**
